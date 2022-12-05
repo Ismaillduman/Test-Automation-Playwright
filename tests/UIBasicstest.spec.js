c7463c69fc4cced4045da8f9435ef626657d5876
Issmailduman <https://github.com/Issmailduman>
2022-12-05 18:33:37 +0100

    child web handel

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -6,7 +6,7 @@ test("First Playwright test", async ({ browser }) => {
   const username = page.locator("#username");
   const password = page.locator("#password");
   const SinginBttn = page.locator("#signInBtn");
-  const cardTitles= page.locator('.card-body a');
+  const cardTitles = page.locator(".card-body a");
 
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
@@ -23,26 +23,62 @@ test("First Playwright test", async ({ browser }) => {
   await username.fill("");
   await username.fill("rahulshettyacademy");
   await password.fill("learning");
-  await Promise.all()
-  await SinginBttn.click(
-    [
-      page.waitForNavigation(), // thats for non server based application
-      SinginBttn.click(),
-
-    ]
-  );
+  await Promise.all([
+    page.waitForNavigation(), // thats for non server based application
+    SinginBttn.click(),
+  ]);
 
   console.log(await cardTitles.first().textContent());
   console.log(await cardTitles.nth(1).textContent());
   console.log(await cardTitles.nth(2).textContent());
   console.log(await cardTitles.last().textContent());
-
 });
-/*test("Page Playwright test", async ({ page }) => {
-  // with 'only' keyword i can run only this test
-  //const context= await browser.newContext(); // these are consist as a default in playwright
-  //const page= await context.newPage();
-  await page.goto("https://www.youtube.com/");
-  console.log(await page.title());
-  await expect(page).toHaveTitle("YouTube");
-});*/
+test("UI Controls", async ({ page }) => {
+  const username = page.locator("#username");
+  const password = page.locator("#password");
+  const SinginBttn = page.locator("#signInBtn");
+  const dropDownBox = page.locator("select.form-control");
+  const radioBtn = page.locator(".radiotextsty");
+  const popup = page.locator("#okayBtn");
+  const checkBox = page.locator("#terms");
+  const documentLink = page.locator("[href*= 'documents-request']");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  await dropDownBox.selectOption("consult");
+  await radioBtn.last().click();
+  await popup.click();
+
+  await checkBox.click();
+  await expect(checkBox).toBeChecked();
+  await checkBox.uncheck();
+  expect(await checkBox.isChecked()).toBeFalsy();
+
+  await expect(documentLink).toHaveAttribute("class", "blinkingText");
+  //documentLink.click();
+});
+
+//to handel child web page on a new tab create a new test block and a newPage
+
+test.only("Child Webpage", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator("[href*= 'documents-request']");
+
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    documentLink.click(),
+  ]);
+
+  const text = await newPage.locator(".im-para.red").textContent();
+  //console.log(text);
+  //out put
+
+  //Please email us at mentor@rahulshettyacademy.com with below template to receive response
+
+  const arrayText = await text.split("@");
+  const domain = await arrayText[1].split(" ")[0];
+  await page.locator("#username").type(domain);
+  await page.pause();
+  console.log(await page.locator("#username").textContent());
+});
