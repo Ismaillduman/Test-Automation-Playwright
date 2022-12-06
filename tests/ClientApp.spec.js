cdf4720f3fc35c789769834794eacc77b5b85493
Issmailduman <https://github.com/Issmailduman>
2022-12-06 16:39:04 +0100

    Iframe

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require("@playwright/test");
-test.only("Client App login", async ({ browser }) => {
+test("Client App login", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
 
