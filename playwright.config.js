91cca6534b0f04a52bf75f4839249d9154b92148
Issmailduman <https://github.com/Issmailduman>
2022-12-08 21:55:56 +0100

    configfile

--- playwright.config.js
+++ playwright.config.js
@@ -51,7 +51,7 @@ const config = {
       use: {
         ...devices['Desktop Chrome'],
         headless: false ,// when i dont wanto use --headed write this code hier
-        screenshot: 'on',
+        screenshot: 'only-on-failure',
         trace: 'retain-on-failure' // off the on to memory
       },
     },
